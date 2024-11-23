67e8cf8b87dbe1a9e1bf744b33de78a8bb117796
BuddhadebKoner <jeetkoner36@gmail.com>
2024-11-23 20:27:03 +0530

    cards

---
+++ buddhadeb/src/data.js
@@ -0,0 +1,54 @@
+const profileData = {
+   profile: {
+      image: "https://avatars.githubusercontent.com/u/113292029?s=400&u=e48bc51e5e7e82942c5c109e95419cc1c781cc42&v=4",
+      name: "Buddhadeb Koner",
+      title: "Web Developer | UI/UX Designer",
+   },
+   bio: {
+      title: "About Me",
+      description:
+         "Passionate developer with 5+ years of experience building responsive web applications and crafting beautiful user interfaces.",
+   },
+   socialMedia: [
+      {
+         title: "LinkedIn",
+         href: "https://www.linkedin.com/in/buddhadeb-koner-8501b3231/",
+         icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/640px-LinkedIn_logo_initials.png",
+         bgColor: "bg-purple-500",
+         hoverColor: "hover:bg-purple-600",
+      },
+      {
+         title: "Twitter",
+         href: "https://x.com/buddhadeb_koner",
+         icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/X_logo.jpg/800px-X_logo.jpg",
+         bgColor: "bg-blue-500",
+         hoverColor: "hover:bg-blue-600",
+      },
+      {
+         title: "Instagram",
+         href: "https://www.instagram.com/buddhadeb_koner",
+         icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/2048px-Instagram_logo_2022.svg.png",
+         bgColor: "bg-gray-500",
+         hoverColor: "hover:bg-gray-600",
+      },
+      {
+         title: "GitHub",
+         href: "https://github.com/BuddhadebKoner",
+         icon: "https://www.svgrepo.com/show/361509/github-logo.svg",
+         bgColor: "bg-gray-500",
+         hoverColor: "hover:bg-gray-600",
+      }, {
+         title: "Portfolio",
+         href: "https://buddhadebkoner.rajislab.com",
+         icon: "https://icons.veryicon.com/png/o/miscellaneous/smart-icon-library/internet-61.png",
+         bgColor: "bg-white",
+         hoverColor: "hover:bg-gray-600",
+      },
+   ],
+   contact: {
+      email: "iambuddhadebkoner@gmail.com",
+      phone: "+91 9339813998",
+   },
+};
+
+export default profileData;
